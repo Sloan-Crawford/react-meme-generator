311601231a906dbdb2a9122cf2adc5be29444dc9
Sloan-Crawford <sloan.crawford@gmail.com>
2022-10-06 10:10:40 -0400

    Update Generator component to display images

--- src/Components/Generator.js
+++ src/Components/Generator.js
@@ -1,33 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import memesData from "../Data/MemesData";
 
 export default function Generator() {
- function handleGetMemeImage() {
+ const [memeImage, setMemeImage] = useState("")
+
+ function GetMemeImage() {
   const memesArray = memesData.data.memes
   const randomNumber = Math.floor(Math.random() * memesArray.length)
   const url = memesArray[randomNumber].url // same as this but with object destructuring:
   // const {url} = memesArray[randomNumber]  // = get the url property from the memesArray.
-  console.log(url);
-  
+  setMemeImage(url)
  }
  
  return (
   <main>
    <div className="g-container">
     <h2 className="g-title">Meme Generator</h2>
-     <div className="g-form">
+    <div className="g-form">
       <div className="input-container">
        <input type="text" placeholder="Top Text"/>
        <input type="text" placeholder="Bottom Text"/>
       </div>
       <div className="submit-container">
-       <button onClick={handleGetMemeImage}>Get a new meme image</button>
+       <button onClick={GetMemeImage}>Get a new meme image</button>
       </div>
-     </div>
    </div>
-   <div className="image-container">
-   {/* <img alt="meme" source={meme} className="meme-image" /> */}
-   </div>
+  </div>
+  <div className="image-container">
+   <img src={memeImage} alt="meme" className="meme-image" />
+  </div>
   </main>
  )
 }
