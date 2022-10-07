b4456d548774c9ae0d728559eb752a288256c0f5
Sloan-Crawford <sloan.crawford@gmail.com>
2022-10-07 11:02:40 -0400

    Added useState guide to Notes.txt

--- src/Components/Generator.js
+++ src/Components/Generator.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import memesData from "../Data/MemesData";
 
 export default function Generator() {
- const [memeImage, setMemeImage] = useState("")
+ const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg")
 
  function GetMemeImage() {
   const memesArray = memesData.data.memes
